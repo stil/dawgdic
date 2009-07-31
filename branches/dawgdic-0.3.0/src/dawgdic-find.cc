474c5510d616b6800a2cdc226727b407c63180ce
susumu.yata <susumu.yata@228e4c24-2f48-11de-bd8d-4b29057232bf>
2009-07-31 13:09:44 +0000

    Added branches for 0.3.0 and 0.3.1. Modified a Japanese wikipage for interfaces.

---
+++ branches/dawgdic-0.3.0/src/dawgdic-find.cc
@@ -0,0 +1,85 @@
+#include "dawgdic/dictionary.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Loads a dictionary file.
+bool LoadDictionary(const char *dic_file_name, dawgdic::Dictionary *dic)
+{
+	std::cerr << "input: " << dic_file_name << std::endl;
+
+	// Opens an input file.
+	std::ifstream dic_file(dic_file_name, std::ios::binary);
+	if (!dic_file.is_open())
+	{
+		std::cerr << "error: failed to open file: "
+			<< dic_file_name << std::endl;
+		return false;
+	}
+
+	if (!dic->Read(&dic_file))
+	{
+		std::cerr << "error: failed to read dictionary from file: "
+			<< dic_file_name << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+// Example of finding prefix keys from each line of an input text.
+// There are two ways to perform prefix matching.
+// One uses dawgdic::DictionaryExplorer,
+// and the other uses only dawgdic::Dictionary.
+bool FindKeys(const dawgdic::Dictionary &dic, std::istream *input)
+{
+	std::vector<std::size_t> lengths;
+	std::string line;
+	while (std::getline(*input, line))
+	{
+		dawgdic::BaseType index = dic.root();
+		for (std::size_t i = 0; i < line.length(); ++i)
+		{
+			if (!dic.Follow(line[i], &index))
+				break;
+
+			// Reads a value.
+			if (dic.has_value(index))
+				lengths.push_back(i + 1);
+		}
+
+		if (lengths.empty())
+			std::cout << line << ": not found" << std::endl;
+		else
+		{
+			std::cout << line << ": found, num = " << lengths.size() << ',';
+			for (std::size_t i = 0; i < lengths.size(); ++i)
+				std::cout << ' ' << lengths[i];
+			std::cout << std::endl;
+		}
+		lengths.clear();
+	}
+
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " DicFile" << std::endl;
+		return 1;
+	}
+
+	const char *dic_file_name = argv[1];
+
+	dawgdic::Dictionary dic;
+	if (!LoadDictionary(dic_file_name, &dic))
+		return 1;
+
+	FindKeys(dic, &std::cin);
+
+	return 0;
+}
---
+++ branches/dawgdic-0.3.1/src/dawgdic-build.cc
@@ -0,0 +1,114 @@
+#include "dawgdic/dawg-builder.h"
+#include "dawgdic/dictionary-builder.h"
+
+#include <fstream>
+#include <iostream>
+
+// Builds a dictionary from a sorted lexicon.
+bool BuildDictionary(const char *lexicon_file_name, dawgdic::Dictionary *dic)
+{
+	std::cerr << "input: " << lexicon_file_name << std::endl;
+
+	// Opens an input file.
+	std::ifstream lexicon_file(lexicon_file_name, std::ios::binary);
+	if (!lexicon_file.is_open())
+	{
+		std::cerr << "error: failed to open file: "
+			<< lexicon_file_name << std::endl;
+		return false;
+	}
+
+	// Reads keys and inserts them into a dawg.
+	dawgdic::DawgBuilder dawg_builder;
+	std::string key;
+	std::size_t key_count = 0;
+	while (std::getline(lexicon_file, key))
+	{
+		if (!dawg_builder.Insert(key.c_str()))
+		{
+			std::cerr << "error: failed to insert key: " << key << std::endl;
+			return false;
+		}
+
+		if (++key_count % 10000 == 0)
+			std::cerr << "no. keys: " << key_count << '\r';
+	}
+	dawgdic::Dawg dawg;
+	dawg_builder.Finish(&dawg);
+
+	std::cerr << "no. keys: " << key_count << std::endl;
+	std::cerr << "no. states: "
+		<< dawg.num_of_states() << std::endl;
+	std::cerr << "no. transitions: "
+		<< dawg.num_of_transitions() << std::endl;
+	std::cerr << "no. merged states: "
+		<< dawg.num_of_merged_states() << std::endl;
+	std::cerr << "no. merging states: "
+		<< dawg.num_of_merging_states() << std::endl;
+	std::cerr << "no. merged transitions: "
+		<< dawg.num_of_merged_transitions() << std::endl;
+
+	// Builds a dictionary from a dawg.
+	dawgdic::BaseType num_of_unused_units;
+	if (!dawgdic::DictionaryBuilder::Build(dawg, dic, &num_of_unused_units))
+	{
+		std::cerr << "error: failed to build dictionary" << std::endl;
+		return false;
+	}
+	double unused_ratio = 100.0 * num_of_unused_units / dic->size();
+
+	std::cerr << "no. elements: " << dic->size() << std::endl;
+	std::cerr << "no. unused elements: " << num_of_unused_units
+		<< " (" << unused_ratio << "%)" << std::endl;
+	std::cerr << "dictionary size: " << dic->total_size() << std::endl;
+
+	return true;
+}
+
+// Writes a dictionary to a file.
+bool SaveDictionary(const dawgdic::Dictionary &dic,
+	const char *dic_file_name)
+{
+	std::cerr << "output: " << dic_file_name << std::endl;
+
+	// Creates an output file.
+	std::ofstream dic_file(dic_file_name, std::ios::binary);
+	if (!dic_file.is_open())
+	{
+		std::cerr << "error: failed to open file: "
+			<< dic_file_name << std::endl;
+		return false;
+	}
+
+	if (!dic.Write(&dic_file))
+	{
+		std::cerr << "error: failed to write dictionary to file: "
+			<< dic_file_name << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	// Checks arguments.
+	if (argc != 3)
+	{
+		std::cerr << "Usage: " << argv[0]
+			<< " LexiconFile DicFile" << std::endl;
+		return 1;
+	}
+
+	const char *lexicon_file_name = argv[1];
+	const char *dic_file_name = argv[2];
+
+	dawgdic::Dictionary dic;
+	if (!BuildDictionary(lexicon_file_name, &dic))
+		return 1;
+
+	if (!SaveDictionary(dic, dic_file_name))
+		return 1;
+
+	return 0;
+}
