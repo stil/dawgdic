556fd23f1dd79a0141ff7ca7a1bd35eeb8b12b3b
Susumu Yata <susumu.yata@gmail.com>
2009-11-08 10:28:45 +0000

    Added template parameters for completing keys in user-defined order.

--- src/dawgdic/ranked-completer-candidate.h
+++ src/dawgdic/ranked-completer-candidate.h
@@ -16,17 +16,33 @@ public:
 	BaseType node_index() const { return node_index_; }
 	ValueType value() const { return value_; }
 
+	template <typename VALUE_COMPARER_TYPE>
 	class Comparer
 	{
 	public:
+		typedef VALUE_COMPARER_TYPE ValueComparerType;
+
+		explicit Comparer(ValueComparerType value_comparer)
+			: value_comparer_(value_comparer) {}
+
 		bool operator()(const RankedCompleterCandidate &lhs,
 			const RankedCompleterCandidate &rhs) const
 		{
 			if (lhs.value() != rhs.value())
-				return lhs.value() < rhs.value();
+				return value_comparer_(lhs.value(), rhs.value());
 			return lhs.node_index() > rhs.node_index();
 		}
+
+	private:
+		ValueComparerType value_comparer_;
 	};
+
+	template <typename VALUE_COMPARER_TYPE>
+	static Comparer<VALUE_COMPARER_TYPE> MakeComparer(
+		VALUE_COMPARER_TYPE value_comparer)
+	{
+		return Comparer<VALUE_COMPARER_TYPE>(value_comparer);
+	}
 
 private:
 	BaseType node_index_;
