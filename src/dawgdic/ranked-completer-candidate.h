b9520ec96ea640f5e6a7d2cc8fcc0494b0f8b56c
susumu.yata <susumu.yata@228e4c24-2f48-11de-bd8d-4b29057232bf>
2012-03-28 07:25:09 +0000

    fixed indentation.

--- src/dawgdic/ranked-completer-candidate.h
+++ src/dawgdic/ranked-completer-candidate.h
@@ -5,50 +5,55 @@
 
 namespace dawgdic {
 
-class RankedCompleterCandidate
-{
-public:
-	RankedCompleterCandidate() : node_index_(0), value_(-1) {}
+class RankedCompleterCandidate {
+ public:
+  RankedCompleterCandidate() : node_index_(0), value_(-1) {}
 
-	void set_node_index(BaseType node_index) { node_index_ = node_index; }
-	void set_value(ValueType value) { value_ = value; }
+  void set_node_index(BaseType node_index) {
+    node_index_ = node_index;
+  }
+  void set_value(ValueType value) {
+    value_ = value;
+  }
 
-	BaseType node_index() const { return node_index_; }
-	ValueType value() const { return value_; }
+  BaseType node_index() const {
+    return node_index_;
+  }
+  ValueType value() const {
+    return value_;
+  }
 
-	template <typename VALUE_COMPARER_TYPE>
-	class Comparer
-	{
-	public:
-		typedef VALUE_COMPARER_TYPE ValueComparerType;
+  template <typename VALUE_COMPARER_TYPE>
+  class Comparer {
+   public:
+    typedef VALUE_COMPARER_TYPE ValueComparerType;
 
-		explicit Comparer(ValueComparerType value_comparer)
-			: value_comparer_(value_comparer) {}
+    explicit Comparer(ValueComparerType value_comparer)
+      : value_comparer_(value_comparer) {}
 
-		bool operator()(const RankedCompleterCandidate &lhs,
-			const RankedCompleterCandidate &rhs) const
-		{
-			if (lhs.value() != rhs.value())
-				return value_comparer_(lhs.value(), rhs.value());
-			return lhs.node_index() > rhs.node_index();
-		}
+    bool operator()(const RankedCompleterCandidate &lhs,
+                    const RankedCompleterCandidate &rhs) const {
+      if (lhs.value() != rhs.value()) {
+        return value_comparer_(lhs.value(), rhs.value());
+      }
+      return lhs.node_index() > rhs.node_index();
+    }
 
-	private:
-		ValueComparerType value_comparer_;
-	};
+   private:
+    ValueComparerType value_comparer_;
+  };
 
-	template <typename VALUE_COMPARER_TYPE>
-	static Comparer<VALUE_COMPARER_TYPE> MakeComparer(
-		VALUE_COMPARER_TYPE value_comparer)
-	{
-		return Comparer<VALUE_COMPARER_TYPE>(value_comparer);
-	}
+  template <typename VALUE_COMPARER_TYPE>
+  static Comparer<VALUE_COMPARER_TYPE> MakeComparer(
+      VALUE_COMPARER_TYPE value_comparer) {
+    return Comparer<VALUE_COMPARER_TYPE>(value_comparer);
+  }
 
-private:
-	BaseType node_index_;
-	ValueType value_;
+ private:
+  BaseType node_index_;
+  ValueType value_;
 
-	// Copyable.
+  // Copyable.
 };
 
 }  // namespace dawgdic
