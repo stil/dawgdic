a8db7b96a196807f09512952d2f463221e71c45c
susumu.yata <susumu.yata@228e4c24-2f48-11de-bd8d-4b29057232bf>
2012-03-28 00:30:24 +0000

    changed indentation.

--- test/dic-test.sh
+++ test/dic-test.sh
@@ -5,35 +5,35 @@ find_bin="../src/dawgdic-find"
 
 if [ ! -f "$build_bin" ]
 then
-	echo "error: $build_bin: not found"
-	exit 1
+  echo "error: $build_bin: not found"
+  exit 1
 fi
 
 if [ ! -f "$find_bin" ]
 then
-	echo "error: $build_bin: not found"
-	exit 1
+  echo "error: $build_bin: not found"
+  exit 1
 fi
 
 ## Builds a dictionary from a lexicon.
 $build_bin -t lexicon lexicon.dic
 if [ $? -ne 0 ]
 then
-	exit 1
+  exit 1
 fi
 
 ## Finds prefix keys from a lexicon.
 $find_bin lexicon.dic < query > dic-result
 if [ $? -ne 0 ]
 then
-	exit 1
+  exit 1
 fi
 
 ## Checks the result.
 cmp dic-result dic-answer
 if [ $? -ne 0 ]
 then
-	exit 1
+  exit 1
 fi
 
 ## Removes temporary files.
