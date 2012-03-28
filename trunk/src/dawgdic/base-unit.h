9e5c6da2dbec49de08c2acc59a712516641db9d5
susumu.yata <susumu.yata@228e4c24-2f48-11de-bd8d-4b29057232bf>
2012-03-28 07:25:09 +0000

    fixed indentation.

--- trunk/src/dawgdic/base-unit.h
+++ trunk/src/dawgdic/base-unit.h
@@ -6,25 +6,36 @@
 namespace dawgdic {
 
 // Unit for building a dawg.
-class BaseUnit
-{
-public:
-	BaseUnit() : base_(0) {}
+class BaseUnit {
+ public:
+  BaseUnit() : base_(0) {}
 
-	// Writes values.
-	void set_base(BaseType base) { base_ = base; }
-	BaseType base() const { return base_; }
+  // Writes values.
+  void set_base(BaseType base) {
+    base_ = base;
+  }
+  BaseType base() const {
+    return base_;
+  }
 
-	// Reads values.
-	BaseType child() const { return base_ >> 2; }
-	bool has_sibling() const { return (base_ & 1) ? true : false; }
-	ValueType value() const { return static_cast<ValueType>(base_ >> 1); }
-	bool is_state() const { return (base_ & 2) ? true : false; }
+  // Reads values.
+  BaseType child() const {
+    return base_ >> 2;
+  }
+  bool has_sibling() const {
+    return (base_ & 1) ? true : false;
+  }
+  ValueType value() const {
+    return static_cast<ValueType>(base_ >> 1);
+  }
+  bool is_state() const {
+    return (base_ & 2) ? true : false;
+  }
 
-private:
-	BaseType base_;
+ private:
+  BaseType base_;
 
-	// Copyable.
+  // Copyable.
 };
 
 }  // namespace dawgdic
--- trunk/src/dawgdic/object-pool.h
+++ trunk/src/dawgdic/object-pool.h
@@ -9,55 +9,59 @@ namespace dawgdic {
 
 // This class works like an array of objects with compact memory management.
 template <typename OBJECT_TYPE, SizeType BLOCK_SIZE = 1 << 10>
-class ObjectPool
-{
-public:
-	typedef OBJECT_TYPE ObjectType;
+class ObjectPool {
+ public:
+  typedef OBJECT_TYPE ObjectType;
 
-	ObjectPool() : blocks_(), size_(0) {}
-	~ObjectPool() { Clear(); }
+  ObjectPool() : blocks_(), size_(0) {}
+  ~ObjectPool() {
+    Clear();
+  }
 
-	// Accessors.
-	ObjectType &operator[](SizeType index)
-	{ return blocks_[index / BLOCK_SIZE][index % BLOCK_SIZE]; }
-	const ObjectType &operator[](SizeType index) const
-	{ return blocks_[index / BLOCK_SIZE][index % BLOCK_SIZE]; }
+  // Accessors.
+  ObjectType &operator[](SizeType index) {
+    return blocks_[index / BLOCK_SIZE][index % BLOCK_SIZE];
+  }
+  const ObjectType &operator[](SizeType index) const {
+    return blocks_[index / BLOCK_SIZE][index % BLOCK_SIZE];
+  }
 
-	// Number of allocated objects.
-	SizeType size() const { return size_; }
+  // Number of allocated objects.
+  SizeType size() const {
+    return size_;
+  }
 
-	// Deletes all objects and frees memory.
-	void Clear()
-	{
-		for (SizeType i = 0; i < blocks_.size(); ++i)
-			delete [] blocks_[i];
+  // Deletes all objects and frees memory.
+  void Clear() {
+    for (SizeType i = 0; i < blocks_.size(); ++i) {
+      delete [] blocks_[i];
+    }
 
-		std::vector<ObjectType *>(0).swap(blocks_);
-		size_ = 0;
-	}
+    std::vector<ObjectType *>(0).swap(blocks_);
+    size_ = 0;
+  }
 
-	// Swaps object pools.
-	void Swap(ObjectPool *pool)
-	{
-		blocks_.swap(pool->blocks_);
-		std::swap(size_, pool->size_);
-	}
+  // Swaps object pools.
+  void Swap(ObjectPool *pool) {
+    blocks_.swap(pool->blocks_);
+    std::swap(size_, pool->size_);
+  }
 
-	// Allocates memory for a new object and returns its ID.
-	SizeType Allocate()
-	{
-		if (size_ == BLOCK_SIZE * blocks_.size())
-			blocks_.push_back(new ObjectType[BLOCK_SIZE]);
-		return size_++;
-	}
+  // Allocates memory for a new object and returns its ID.
+  SizeType Allocate() {
+    if (size_ == BLOCK_SIZE * blocks_.size()) {
+      blocks_.push_back(new ObjectType[BLOCK_SIZE]);
+    }
+    return size_++;
+  }
 
-private:
-	std::vector<ObjectType *> blocks_;
-	SizeType size_;
+ private:
+  std::vector<ObjectType *> blocks_;
+  SizeType size_;
 
-	// Disallows copies.
-	ObjectPool(const ObjectPool &);
-	ObjectPool &operator=(const ObjectPool &);
+  // Disallows copies.
+  ObjectPool(const ObjectPool &);
+  ObjectPool &operator=(const ObjectPool &);
 };
 
 }  // namespace dawgdic
