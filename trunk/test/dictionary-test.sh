6af2affbf205188e2888cdf80385515fd34ac66b
susumu.yata <susumu.yata@228e4c24-2f48-11de-bd8d-4b29057232bf>
2012-09-15 05:20:14 +0000

    renamed test files and updated aclocal.m4.

--- trunk/test/dictionary-test.sh
+++ trunk/test/dictionary-test.sh
@@ -17,25 +17,25 @@ then
 fi
 
 ## Builds a dictionary from a lexicon.
-$build_bin -gt "${test_dir}/lexicon" lexicon.dic
+$build_bin -t "${test_dir}/lexicon" lexicon.dic
 if [ $? -ne 0 ]
 then
   exit 1
 fi
 
 ## Finds prefix keys from a lexicon.
-$find_bin -g lexicon.dic < "${test_dir}/query" > guide-result
+$find_bin lexicon.dic < "${test_dir}/query" > dictionary-result
 if [ $? -ne 0 ]
 then
   exit 1
 fi
 
 ## Checks the result.
-cmp guide-result "${test_dir}/guide-answer"
+cmp dictionary-result "${test_dir}/dictionary-answer"
 if [ $? -ne 0 ]
 then
   exit 1
 fi
 
 ## Removes temporary files.
-rm -f lexicon.dic guide-result
+rm -f lexicon.dic dictionary-result
