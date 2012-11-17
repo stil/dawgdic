acc4c60413315a3e663af68cafe7f510ea098db2
susumu.yata <susumu.yata@228e4c24-2f48-11de-bd8d-4b29057232bf>
2012-11-17 13:49:25 +0000

    Update a testing shell script for out-of-tree build (Issue #10).

--- test/dawg-builder-test.sh
+++ test/dawg-builder-test.sh
@@ -4,7 +4,7 @@ build_bin="${TOP_BUILDDIR:-..}/src/dawgdic-build"
 find_bin="${TOP_BUILDDIR:-..}/src/dawgdic-find"
 test_dir="${TOP_SRCDIR:-..}/test"
 
-"${test_dir}/dawg-builder-test"
+./dawg-builder-test
 if [ $? -ne 0 ]
 then
   exit 1
